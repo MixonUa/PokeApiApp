ec4c257fcb54e1f0cf3333995cbb1a38ed3b04d3
MixonUa <mykhailofrolov@gmail.com>
2023-09-12 13:01:53 +0300

    add download manager

--- PokeApiApp/Controllers/MainViewController.swift
+++ PokeApiApp/Controllers/MainViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 class MainViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var pokemonNamesTableView: UITableView!
     
     let storageManager = PersonalStorageManager()
+    let downloadManager = NetworkFetchService()
+    var pokemonNames: [PokemonNamesModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pokemonNamesTableView.dataSource = self
+    
+        updatePersonInfo()
+        loadPokeNames()
+        print(pokemonNames)
+    }
+    
+    private func updatePersonInfo() {
         nameLabel.text = storageManager.string(for: .name)
         countryLabel.text = storageManager.string(for: .country)
     }
     
-
+    private func loadPokeNames() {
+        downloadManager.requestPokemonNames(completion: { (data, error) in
+            if let data = data {
+                self.pokemonNames = data
+                self.pokemonNamesTableView.reloadData()
+            }
+        })
+    }
     /*
     // MARK: - Navigation
 
@@ -30,4 +48,22 @@ class MainViewController: UIViewController {
     }
     */
 
+}
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonNames.isEmpty ? 1 : pokemonNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = pokemonNamesTableView.dequeueReusableCell(withIdentifier: "NamesCell", for: indexPath) as! NamesCell
+        if pokemonNames.isEmpty {
+            cell.updateCell(name: "No data")
+        } else {
+            cell.updateCell(name: pokemonNames[indexPath.row].name)
+        }
+        return cell
+    }
+    
+    
 }
